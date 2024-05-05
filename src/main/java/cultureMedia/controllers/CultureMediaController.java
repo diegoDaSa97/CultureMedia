fa3dab227ebfd9482aa9d65d2ec8ad22854eef3f
diegoDaSa97 <dalexander.sanchez@udea.edu.co>
2024-05-05 13:17:00 -0500

    Adding–controller

--- src/main/java/cultureMedia/controllers/CultureMediaController.java
+++ src/main/java/cultureMedia/controllers/CultureMediaController.java
@@ -1,25 +1,25 @@
-package cultureMedia.controllers;
+package cultureMedia.controllers
 
-import java.util.*;
+import java.util.*
 
-import cultureMedia.exception.VideoNotFoundException;
-import cultureMedia.model.Video;
-import cultureMedia.service.impl.CultureMediaServiceImpl;
+import cultureMedia.exception.VideoNotFoundException
+import cultureMedia.model.Video
+import cultureMedia.service.impl.CultureMediaServiceImpl
 
 public class CultureMediaController {
 
-    private final CultureMediaServiceImpl cultureMediaService;
+    private final CultureMediaServiceImpl cultureMediaService
 
 
     public CultureMediaController(CultureMediaServiceImpl cultureMediaService) {
-        this.cultureMediaService = cultureMediaService;
+        this.cultureMediaService = cultureMediaService
     }
 
 
     public List<Video> find_allVideos() throws VideoNotFoundException {
-        List<Video> videos = null;
+        List<Video> videos = null
         videos = cultureMediaService.findAll()
-        return videos;
+        return videos
     }
 
 
