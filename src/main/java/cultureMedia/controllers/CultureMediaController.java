2012d970957bceae4384e0e99730aaa821b4dd3c
diegoDaSa97 <dalexander.sanchez@udea.edu.co>
2024-05-15 00:23:48 -0500

    R12-Adding-Components-Rest-API

--- src/main/java/cultureMedia/controllers/CultureMediaController.java
+++ src/main/java/cultureMedia/controllers/CultureMediaController.java
@@ -1,11 +1,24 @@
 package cultureMedia.controllers;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import cultureMedia.exception.VideoNotFoundException;
 import cultureMedia.model.Video;
+import cultureMedia.model.View;
+import cultureMedia.repository.ViewsRepository;
+import cultureMedia.repository.impl.VideoRepositoryImpl;
 import cultureMedia.service.CultureMediaService;
+import cultureMedia.service.impl.CultureMediaServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class CultureMediaController {
 
     private final CultureMediaService cultureMediaService;
@@ -15,11 +28,25 @@ public class CultureMediaController {
         this.cultureMediaService = cultureMediaService;
     }
 
-
-    public List<Video> findAllVideos() throws VideoNotFoundException {
-        List<Video> videos = cultureMediaService.findAll();
-        return cultureMediaService.findAll();
+    public CultureMediaController() {this.cultureMediaService = new CultureMediaServiceImpl(new VideoRepositoryImpl(), new ViewsRepository() {
+        @Override
+        public View save(View save) {
+            return null;
+        }
+    });
     }
+    @GetMapping("/videos")
+    public ResponseEntity<List<Video>> findAllVideos() {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(cultureMediaService.findAll());
+        }
+        catch (VideoNotFoundException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).header("Error-Message", e.getMessage()).body( Collections.emptyList() );
+            }
+        }
 
-
+    @PostMapping("/videos")
+    public Video addVideo(@RequestBody Video video){
+        return cultureMediaService.save(video);
+    }
 }
